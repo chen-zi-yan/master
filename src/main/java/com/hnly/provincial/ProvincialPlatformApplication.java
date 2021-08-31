89acf229782e6527143207d0d6067e92eec136c7
vanish <vanishxiaoma@outlook.com>
2021-08-31 13:07:07 +0800

    init

---
+++ src/main/java/com/hnly/provincial/ProvincialPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hnly.provincial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvincialPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvincialPlatformApplication.class, args);
+    }
+
+}

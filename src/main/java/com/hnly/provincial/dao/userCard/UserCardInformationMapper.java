6f018f459d9ee509134bcd5fb7f4235532d19d8c
陈子岩 <17638290842@163.com>
2021-09-07 17:15:05 +0800

    代码修改

---
+++ src/main/java/com/hnly/provincial/dao/userCard/UserCardInformationMapper.java
@@ -0,0 +1,16 @@
+package com.hnly.provincial.dao.userCard;
+
+import com.hnly.provincial.entity.userCard.UserCardInformation;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 农户卡信息表 Mapper 接口
+ * </p>
+ *
+ * @author czy
+ * @since 2021-09-07
+ */
+public interface UserCardInformationMapper extends BaseMapper<UserCardInformation> {
+
+}

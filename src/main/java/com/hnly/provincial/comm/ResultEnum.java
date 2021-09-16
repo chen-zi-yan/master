62a6a855e681555175aea93ac3c34c18c5d3d756
陈子岩 <17638290842@163.com>
2021-09-16 13:39:32 +0800

    修改内容:清理多余自定义异常

--- src/main/java/com/hnly/provincial/comm/ResultEnum.java
+++ src/main/java/com/hnly/provincial/comm/ResultEnum.java
@@ -32,9 +32,7 @@ public enum ResultEnum {
     CODE_SUPERIOR_EXIST(40016, "无上级单位"),
     CODENOTEMPTY(40017, "行政区划代码不能为空"),
     IDCODE_EXIST(40018, "该身份证号已经存在"),
-    IC_EXIST(40019, "IC已存在"),
-    GETUSERREGISTRATIONNO_EXIST(40020, "该农户编号已存在"),
-    DEVSN_EXIST(40021, "该设备序列号已存在"),
+    DEVSN_EXIST(40019, "该设备序列号已存在"),
 
     ;
     /**

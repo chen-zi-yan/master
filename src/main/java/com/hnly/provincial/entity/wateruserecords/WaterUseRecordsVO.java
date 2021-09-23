6c2f1472b558535f7427d67b5b6f2506cd01d7fb
陈子岩 <17638290842@163.com>
2021-09-23 11:15:42 +0800

    修改內容:删除定义的typeName,查询出来的数据需要的市县乡村,设备类型和农户的名字

--- src/main/java/com/hnly/provincial/entity/wateruserecords/WaterUseRecordsVO.java
+++ src/main/java/com/hnly/provincial/entity/wateruserecords/WaterUseRecordsVO.java
@@ -34,6 +34,24 @@ public class WaterUseRecordsVO extends PageWhere<WaterUseRecords> implements Ser
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
+    @Schema(description = "市名称")
+    private String cityName;
+
+    @Schema(description = "县名称")
+    private String countyName;
+
+    @Schema(description = "乡名称")
+    private String townshipName;
+
+    @Schema(description = "村名称")
+    private String villageName;
+
+    @Schema(description = "农户名称")
+    private String farmerName;
+
+    @Schema(description = "设备类型")
+    private String deviceName;
+
     @Schema(description = "订单号")
     private String orderNo;
 
@@ -54,9 +72,6 @@ public class WaterUseRecordsVO extends PageWhere<WaterUseRecords> implements Ser
 
     @Schema(description = "0 标准计量 1 非标准计量")
     private String type;
-
-    @Schema(description = "0 标准计量 1 非标准计量")
-    private String typeName;
 
     @Schema(description = "开始使用时间")
     private Date startTime;
@@ -91,14 +106,4 @@ public class WaterUseRecordsVO extends PageWhere<WaterUseRecords> implements Ser
     @Schema(description = "三阶费用")
     private BigDecimal useMoneyThird;
 
-    public String getTypeName() {
-        switch (type) {
-            case "0":
-                return "标准计量";
-            case "1":
-                return "非标准计量";
-            default:
-                return "空";
-        }
-    }
 }
--- src/main/java/com/hnly/provincial/service/wateruserecords/impl/WaterUseRecordsServiceImpl.java
+++ src/main/java/com/hnly/provincial/service/wateruserecords/impl/WaterUseRecordsServiceImpl.java
@@ -1,17 +1,24 @@
 package com.hnly.provincial.service.wateruserecords.impl;
 
 import com.alibaba.druid.util.StringUtils;
-import com.hnly.provincial.comm.utils.TableDataUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hnly.provincial.comm.utils.Conversion;
+import com.hnly.provincial.comm.utils.TableDataUtils;
+import com.hnly.provincial.dao.wateruserecords.WaterUseRecordsMapper;
+import com.hnly.provincial.entity.device.Device;
+import com.hnly.provincial.entity.farmer.Farmer;
 import com.hnly.provincial.entity.wateruserecords.WaterUseRecords;
 import com.hnly.provincial.entity.wateruserecords.WaterUseRecordsVO;
-import com.hnly.provincial.dao.wateruserecords.WaterUseRecordsMapper;
+import com.hnly.provincial.service.area.IAreaService;
+import com.hnly.provincial.service.device.IDeviceService;
+import com.hnly.provincial.service.farmer.IFarmerService;
 import com.hnly.provincial.service.wateruserecords.IWaterUseRecordsService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -24,14 +31,31 @@ import java.util.List;
 @Service
 public class WaterUseRecordsServiceImpl extends ServiceImpl<WaterUseRecordsMapper, WaterUseRecords> implements IWaterUseRecordsService {
 
+    @Resource
+    private IAreaService iAreaService;
+
+    @Resource
+    private IFarmerService farmerService;
+
+    @Resource
+    private IDeviceService deviceService;
+
     @Override
     public TableDataUtils<List<WaterUseRecordsVO>> findListByPage(WaterUseRecordsVO waterUseRecordsVO) {
         Page<WaterUseRecords> page = lambdaQuery()
                 .likeRight(StringUtils.isEmpty(waterUseRecordsVO.getCode()), WaterUseRecords::getCode, waterUseRecordsVO.getCode())
                 .page(waterUseRecordsVO.page());
         List<WaterUseRecordsVO> waterUseRecordsVOs = Conversion.changeList(page.getRecords(), WaterUseRecordsVO.class);
-        for (WaterUseRecordsVO useRecordsVO : waterUseRecordsVOs) {
-            System.out.println("useRecordsVO = " + useRecordsVO);
+        for (WaterUseRecordsVO vo : waterUseRecordsVOs) {
+            Farmer farmer = farmerService.lambdaQuery().eq(vo.getFarmerId() != null, Farmer::getId, vo.getFarmerId()).last("limit 1").one();
+            vo.setFarmerName(farmer.getName());
+            Device device = deviceService.lambdaQuery().eq(vo.getDeviceId() != null, Device::getId, vo.getDeviceId()).last("limit 1").one();
+            vo.setDeviceName(device.getName());
+            Map<String, String> allAreaName = iAreaService.getAllAreaName(vo.getCode());
+            vo.setCityName(allAreaName.get("shi"));
+            vo.setCountyName(allAreaName.get("xian"));
+            vo.setTownshipName(allAreaName.get("xiang"));
+            vo.setVillageName(allAreaName.get("cun"));
         }
 
         return TableDataUtils.success(page.getTotal(), waterUseRecordsVOs);
